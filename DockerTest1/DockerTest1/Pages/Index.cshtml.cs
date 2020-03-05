8d23fd956493c11b758b110ccb4f19de81789381
Ateet Agarwal <ateet.agarwal@nagarro.com>
2020-03-05 22:12:52 +0530

    Delete tar file of image

---
+++ DockerTest1/DockerTest1/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Web;
+
+namespace DockerTest1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string IP = string.Empty;
+        public void OnGet()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    IP = ip.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ DockerTest1/DockerTest1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Web;
+
+namespace DockerTest1
+{
+    public class Program
+    {
+       static string IP = string.Empty;
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
